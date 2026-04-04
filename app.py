b62da66447699a791fdf2fdf7142447aa936c4d3
boginilasyalasya-ai <boginilasyalasya@gmail.com>
2026-04-04 15:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load trained model
+
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Title
+
 st.title("💻 Laptop Price Predictor")
 
-# User inputs
+
 ram = st.selectbox("Select RAM (GB)", [4, 8, 16, 32])
 storage = st.selectbox("Select Storage (GB)", [256, 512, 1024])
 ssd = st.selectbox("SSD (0 = No, 1 = Yes)", [0, 1])
 
-# Button
+
 if st.button("Predict Price"):
     input_data = np.array([[ram, storage, ssd]])
     prediction = model.predict(input_data)
