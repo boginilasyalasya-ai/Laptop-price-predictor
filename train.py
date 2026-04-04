7d8014e6598a839d18ea239715bb1d0e4a846fee
boginilasyalasya-ai <boginilasyalasya@gmail.com>
2026-04-04 15:27:56 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-# Sample dataset
+
 data = {
     'RAM': [4, 8, 16, 32],
     'Storage': [256, 512, 512, 1024],
@@ -10,18 +10,16 @@ data = {
     'Price': [30000, 50000, 75000, 120000]
 }
 
-# Convert to DataFrame
 df = pd.DataFrame(data)
 
-# Features and target
 X = df[['RAM', 'Storage', 'SSD']]
 y = df['Price']
 
-# Model training
+
 model = LinearRegression()
 model.fit(X, y)
 
-# Save model
+
 pickle.dump(model, open('model.pkl', 'wb'))
 
 print("Model trained and saved successfully!")
